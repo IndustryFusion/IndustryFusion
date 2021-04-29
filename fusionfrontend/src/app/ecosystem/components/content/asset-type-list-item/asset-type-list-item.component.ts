c2c928f0de2c7cc408ea44aaef56d6eb80fbbe26
Johannes Sylupp <johannes.sylupp@iteratec.com>
2021-04-29 17:43:38 +0200

    IF-170: Add store for AssetTypeDetails (counts of Asset series, Asset instances and asset templates)

--- fusionfrontend/src/app/ecosystem/components/content/asset-type-list-item/asset-type-list-item.component.ts
+++ fusionfrontend/src/app/ecosystem/components/content/asset-type-list-item/asset-type-list-item.component.ts
@@ -20,6 +20,10 @@ import { BaseListItemComponent } from '../base/base-list-item/base-list-item.com
 import { AssetType } from '../../../../store/asset-type/asset-type.model';
 import { AssetTypeService } from '../../../../store/asset-type/asset-type.service';
 import {AssetTypesComposedQuery} from "../../../../store/composed/asset-types-composed.query";
+import {AssetTypeDetailsQuery} from "../../../../store/asset-type-details/asset-type-details.query";
+import {AssetTypeDetails} from "../../../../store/asset-type-details/asset-type-details.model";
+import {Observable} from "rxjs";
+import {AssetTypeDetailsService} from "../../../../store/asset-type-details/asset-type-details.service";
 
 @Component({
   selector: 'app-asset-type-list-item',
@@ -30,6 +34,9 @@ export class AssetTypeListItemComponent extends BaseListItemComponent implements
 
   @Input()
   item: AssetType;
+
+  itemDetails$: Observable<AssetTypeDetails>;
+
   templateCount: number;
   assetSeriesCount: number;
   assetInstancesCount: number;
@@ -37,13 +44,23 @@ export class AssetTypeListItemComponent extends BaseListItemComponent implements
   constructor(public route: ActivatedRoute,
               public router: Router,
               public assetTypeService: AssetTypeService,
+              private assetTypeDetailsService : AssetTypeDetailsService,
+              private assetTypeDetailsQuery: AssetTypeDetailsQuery,
               private assetTypesComposedQuery: AssetTypesComposedQuery) {
     super(route, router, assetTypeService);
   }
 
   ngOnInit() {
-    // TODO: Call resolver?
-    // TODO: Fix: Call via URL yields no data
+    // TODO (js): Call resolver?
+    // TODO (js): Bug: Call via URL yields no data
+
+    this.assetTypeDetailsService.getAssetTypeDetails(this.item.id).subscribe();
+
+    let test: number;
+    this.itemDetails$ = this.assetTypeDetailsQuery.selectDetailsOfAssetType(this.item.id);
+    this.itemDetails$.subscribe(x => {test = x.assetSeriesCount; console.log(test);});
+
+
     this.assetTypesComposedQuery.selectAssetTypeWithTemplateCount(this.item.id).subscribe(x => this.templateCount = x.templateCount);
     this.assetSeriesCount = 9999;
     this.assetInstancesCount = 9999;
---
+++ fusionfrontend/src/app/store/asset-type-details/asset-type-details.service.ts
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ID} from '@datorama/akita';
+import {tap} from 'rxjs/operators';
+import {environment} from '../../../environments/environment';
+import {AssetTypeDetails} from "./asset-type-details.model";
+import {AssetTypeDetailsStore} from "./asset-type-details.store";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssetTypeDetailsService {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private assetTypeDetailsStore: AssetTypeDetailsStore, private http: HttpClient) { }
+
+  getAssetTypeDetails (assetTypeId: ID): Observable<AssetTypeDetails> {
+    const path = `assettypes/${assetTypeId}/details`;
+    return this.assetTypeDetailsStore.cachedById(assetTypeId, this.http.get<AssetTypeDetails>(`${environment.apiUrlPrefix}/${path}`, this.httpOptions)
+      .pipe(tap(entity => {
+        this.assetTypeDetailsStore.upsertCached(entity);
+      })));
+  }
+
+  setActive(assetTypeId: ID) {
+    this.assetTypeDetailsStore.setActive(assetTypeId);
+  }
+}
