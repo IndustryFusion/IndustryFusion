db8353ffc4ebe066ea6c03ab589b2024dfb5389c
Samuel Sedlmeir <Samuel.Sedlmeir@iteratec.com>
2021-07-12 09:10:14 +0200

    IF-296 improve error toast styling

--- fusionfrontend/src/app/services/error.interceptor.ts
+++ fusionfrontend/src/app/services/error.interceptor.ts
@@ -8,14 +8,23 @@ import { MessageService } from 'primeng/api';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
+  ERROR_TOAST_DURATION = 500000;
+
   constructor(private messageService: MessageService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error) => {
-        const errorMessage = error.error?.message ? error.error.message : error.message;
-        this.messageService.add(({ severity: 'error', summary: 'Error', detail: errorMessage, life: 500000 }));
+        console.warn(error);
+        const errorMessage = error.error?.error ? `${error.error?.error} (${error.error?.status})` : error.statusText;
+        const errorSummary = error.error?.message ? error.error.message : error.message;
+        this.messageService.add(({
+          severity: 'info',
+          summary: errorSummary,
+          detail: errorMessage,
+          life: this.ERROR_TOAST_DURATION
+        }));
         return throwError(error.message);
       })
     );
