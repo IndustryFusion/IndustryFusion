01d11cbbf1117cf3430bc8f7583697860620f197
Samuel Sedlmeir <Samuel.Sedlmeir@iteratec.com>
2021-08-03 10:04:46 +0200

    IF-137: add second progress column to maintenance dashboard

--- fusionfrontend/src/app/dashboards/components/content/maintenance-list/maintenance-progressbar/maintenance-progressbar.component.ts
+++ fusionfrontend/src/app/dashboards/components/content/maintenance-list/maintenance-progressbar/maintenance-progressbar.component.ts
@@ -4,8 +4,6 @@ import { FactoryAssetDetailsWithFields } from 'src/app/store/factory-asset-detai
 const MAINTENANCE_FIELD_NAME = 'Hours till maintenance';
 const MAXIMAL_MAINTENANCE_VALUE = 1500;
 const HOURS_PER_DAY = 24;
-const DAY_PER_WEEK = 7;
-const DAYS_PER_MONTH = 30.4167;
 
 @Component({
   selector: 'app-maintenance-progressbar',
@@ -20,12 +18,11 @@ export class MaintenanceProgressbarComponent implements OnInit {
   maintenanceFieldIndex: number;
   maintenanceDurationHours: number;
   maintenanceDurationDays: number;
-  maintenanceDurationWeeks: number;
-  maintenanceDurationMonth: number;
   maintenancePercentage: number;
-  noMaintenacneValue: boolean;
+  isMaintenanceValueAvailable: boolean;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.maintenanceFieldIndex = this.asset.fields.findIndex(field => field.name === MAINTENANCE_FIELD_NAME);
@@ -33,11 +30,9 @@ export class MaintenanceProgressbarComponent implements OnInit {
       this.maintenanceDurationHours = Number(this.asset.fields[this.maintenanceFieldIndex].value);
       this.maintenancePercentage = (this.maintenanceDurationHours / MAXIMAL_MAINTENANCE_VALUE) * 100;
       this.maintenanceDurationDays = Math.round(this.maintenanceDurationHours / HOURS_PER_DAY);
-      this.maintenanceDurationWeeks = Math.round(this.maintenanceDurationHours / (HOURS_PER_DAY * DAY_PER_WEEK));
-      this.maintenanceDurationMonth = Math.round(this.maintenanceDurationHours / (DAYS_PER_MONTH * HOURS_PER_DAY));
-      this.noMaintenacneValue = false;
+      this.isMaintenanceValueAvailable = true;
     } else {
-      this.noMaintenacneValue = true;
+      this.isMaintenanceValueAvailable = false;
     }
   }
 
