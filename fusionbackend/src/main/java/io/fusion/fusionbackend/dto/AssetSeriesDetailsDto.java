3f51f0a3edfffefd15f17290f0ed0b0abd59cacb
Johannes Sylupp <johannes.sylupp@iteratec.com>
2021-08-12 14:42:01 +0200

    IF-301: Fix pmd violations

--- fusionbackend/src/main/java/io/fusion/fusionbackend/dto/AssetSeriesDetailsDto.java
+++ fusionbackend/src/main/java/io/fusion/fusionbackend/dto/AssetSeriesDetailsDto.java
@@ -17,12 +17,8 @@ package io.fusion.fusionbackend.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
-
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 @Data
 @SuperBuilder
