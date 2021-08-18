941373f1747cb51b0d169f12ceea4a06faf133db
Johannes Sylupp <johannes.sylupp@iteratec.com>
2021-08-18 10:30:37 +0200

    IF-338: Refactoring of connectivity settings

--- fusionfrontend/src/app/fleet/components/content/asset-series-wizard/asset-series-wizard-connectivity-settings/asset-series-wizard-connectivity-settings.component.ts
+++ fusionfrontend/src/app/fleet/components/content/asset-series-wizard/asset-series-wizard-connectivity-settings/asset-series-wizard-connectivity-settings.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ConnectivityTypeQuery } from '../../../../../store/connectivity-type/connectivity-type.query';
 import { ConnectivityProtocol, ConnectivityType } from '../../../../../store/connectivity-type/connectivity-type.model';
-import { ID } from '@datorama/akita';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AssetSeriesWizardConnectivitySettingsTooltipComponent } from './asset-series-wizard-connectivity-settings-tooltip/asset-series-wizard-connectivity-settings-tooltip.component';
 import { AssetSeries } from '../../../../../store/asset-series/asset-series.model';
@@ -33,13 +32,9 @@ export class AssetSeriesWizardConnectivitySettingsComponent implements OnInit {
 
   ngOnInit(): void {
     this.createFormGroup();
-    this.selectFirstItemsInDropdowns();
-    this.disableFormGroupOnEdit();
+    this.updateSelectOptionsAndInfoText();
+    this.disableFormGroupOnEditMode();
     this.assetSeriesForm.addControl('connectivitySettings', this.connectivitySettingsForm);
-  }
-
-  public isEditMode(): boolean {
-    return this.mode === DialogType.EDIT;
   }
 
   private createFormGroup(): void {
@@ -49,18 +44,16 @@ export class AssetSeriesWizardConnectivitySettingsComponent implements OnInit {
       connectivityTypeId: [null, Validators.required],
       connectivityProtocolId: [null, Validators.required],
       connectionString: [null, requiredTextValidator],
-   /*   connectivityTypeId: [{ value: null, disabled: this.isEditMode() }, Validators.required],
-      connectivityProtocolId: [{ value: null, disabled: this.isEditMode() }, Validators.required],
-      connectionString: [{ value: null, disabled: this.isEditMode() }, requiredTextValidator],*/
     });
     this.connectivitySettingsForm.valueChanges.subscribe(() => this.valid.emit(this.connectivitySettingsForm.valid));
 
-    if (this.assetSeries?.connectivitySettings) {
-      this.connectivitySettingsForm.patchValue(this.assetSeries.connectivitySettings);
+    this.connectivitySettingsForm.patchValue(this.assetSeries.connectivitySettings);
+    if (this.mode === DialogType.CREATE) {
+      this.selectFirstItemsInDropdowns();
     }
   }
 
-  private disableFormGroupOnEdit() {
+  private disableFormGroupOnEditMode() {
     if (this.mode === DialogType.EDIT) {
       this.connectivitySettingsForm.get('connectivityTypeId').disable( { onlySelf: true });
       this.connectivitySettingsForm.get('connectivityProtocolId').disable( { onlySelf: true });
@@ -70,19 +63,21 @@ export class AssetSeriesWizardConnectivitySettingsComponent implements OnInit {
 
   private selectFirstItemsInDropdowns(): void {
     this.connectivitySettingsForm.get('connectivityTypeId').setValue(1);
-    this.onChangeConnectivityType(1);
   }
 
-  onChangeConnectivityType(connectivityTypeId: ID): void {
+  private updateSelectOptionsAndInfoText(): void {
+    const connectivityTypeId = this.connectivitySettingsForm.get('connectivityTypeId').value;
+
     if (connectivityTypeId && this.connectivityTypeOptions) {
       const selectedConnectivityType = this.connectivityTypeOptions
         .find(connectivityType => String(connectivityType.id) === String(connectivityTypeId));
+
       this.connectivityProtocolOptions = selectedConnectivityType.availableProtocols;
       this.infoText = selectedConnectivityType.infoText;
 
       if (this.connectivityProtocolOptions.length > 0) {
         this.connectivitySettingsForm.get('connectivityProtocolId').setValue(this.connectivityProtocolOptions[0].id);
-        this.onChangeProtocolType(this.connectivityProtocolOptions[0].id);
+        this.updateConnectivityProtocolOptionsAndConnectionString();
 
       } else {
         this.connectivitySettingsForm.get('connectivityProtocolId').setValue(null);
@@ -91,7 +86,17 @@ export class AssetSeriesWizardConnectivitySettingsComponent implements OnInit {
     }
   }
 
-  onChangeProtocolType(connectivityProtocolId: ID): void {
+  onChangeConnectivityType(): void {
+    this.updateSelectOptionsAndInfoText();
+  }
+
+  onChangeProtocolType(): void {
+    this.updateConnectivityProtocolOptionsAndConnectionString();
+  }
+
+  private updateConnectivityProtocolOptionsAndConnectionString() {
+    const connectivityProtocolId = this.connectivitySettingsForm.get('connectivityProtocolId').value;
+
     if (connectivityProtocolId && this.connectivityProtocolOptions) {
       const connectionString = this.connectivityProtocolOptions
         .find(connectivityProtocol => String(connectivityProtocol.id) === String(connectivityProtocolId)).connectionStringPattern;
