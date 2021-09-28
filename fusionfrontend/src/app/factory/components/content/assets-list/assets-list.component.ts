3eb7903886fd1e1ad1fabb598efcc3e0f32e1678
florian.paul <florian.paul@iteratec.com>
2021-09-28 10:35:57 +0200

    IF-425 Modify asset list to primeng table

--- fusionfrontend/src/app/factory/components/content/assets-list/assets-list.component.ts
+++ fusionfrontend/src/app/factory/components/content/assets-list/assets-list.component.ts
@@ -23,16 +23,20 @@ import { Room } from 'src/app/store/room/room.model';
 import { FactoryAssetDetails, FactoryAssetDetailsWithFields, AssetModalMode, AssetModalType } from '../../../../store/factory-asset-details/factory-asset-details.model';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { AssetWithFields } from '../../../../store/asset/asset.model';
+import { Asset, AssetWithFields } from '../../../../store/asset/asset.model';
 import { AssetInstantiationComponent } from '../asset-instantiation/asset-instantiation.component';
 import { Location } from '@angular/common';
 import { WizardHelper } from '../../../../common/utils/wizard-helper';
+import { ConfirmationService, MenuItem } from 'primeng/api';
+import { FilterOption, FilterType } from '../../../../components/ui/table-filter/filter-options';
+import { faTimes, faWrench, faThLarge, faReply, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-assets-list',
   templateUrl: './assets-list.component.html',
   styleUrls: ['./assets-list.component.scss'],
-  providers: [DialogService]
+  providers: [DialogService, ConfirmationService]
 })
 export class AssetsListComponent implements OnInit {
   @Input()
@@ -50,13 +54,27 @@ export class AssetsListComponent implements OnInit {
   @Input()
   room: Room;
   @Output()
-  selectedEvent = new EventEmitter<Set<ID>>();
+  selectedEvent = new EventEmitter<ID[]>();
   @Output()
   toolBarClickEvent = new EventEmitter<string>();
   @Output()
-  assetDetailsSelected = new EventEmitter<FactoryAssetDetails>();
-  @Output()
   updateAssetEvent = new EventEmitter<[Room, FactoryAssetDetails]>();
+
+  displayedFactoryAssets: FactoryAssetDetailsWithFields[];
+  filteredFactoryAssets: FactoryAssetDetailsWithFields[];
+  searchedFactoryAssets: FactoryAssetDetailsWithFields[];
+  selectedFactoryAssets: FactoryAssetDetailsWithFields[] = [];
+  menuActions: MenuItem[];
+  activeListItem: FactoryAssetDetailsWithFields;
+  faTimes = faTimes;
+  faWrench = faWrench;
+  faThLarge = faThLarge;
+  faCheckCircle = faCheckCircle;
+  faTrashAlt = faTrashAlt;
+  faReply = faReply;
+  faHome = faHome;
+  shouldShowDeleteItem = false;
+
 
   asset: AssetWithFields;
   assetDetailsForm: FormGroup;
@@ -64,10 +82,8 @@ export class AssetsListComponent implements OnInit {
   ref: DynamicDialogRef;
 
   isLoading$: Observable<boolean>;
-  selectedIds: Set<ID> = new Set();
-  filterDict: { [key: string]: string[]; };
 
-  assetsMapping:
+  titleMapping:
     { [k: string]: string } = { '=0': 'No assets', '=1': '# Asset', other: '# Assets' };
 
   editBarMapping:
@@ -77,16 +93,38 @@ export class AssetsListComponent implements OnInit {
       other: '# Assets selected'
     };
 
+  possibleFilters: FilterOption[] = [{ filterType: FilterType.DROPDOWNFILTER, columnName: 'Category', attributeToBeFiltered: 'category' },
+    { filterType: FilterType.DROPDOWNFILTER, columnName: 'Manufacturer', attributeToBeFiltered: 'manufacturer' },
+    { filterType: FilterType.DROPDOWNFILTER, columnName: 'Room', attributeToBeFiltered: 'roomName' },
+    { filterType: FilterType.DROPDOWNFILTER, columnName: 'Factory Site', attributeToBeFiltered: 'factorySiteName'}];
+
   constructor(
     private assetService: AssetService,
     private formBuilder: FormBuilder,
     public dialogService: DialogService,
+    private confirmationService: ConfirmationService,
     private routingLocation: Location) {
       this.createDetailsAssetForm(this.formBuilder);
   }
 
   ngOnInit() {
+    this.displayedFactoryAssets = this.factoryAssetDetailsWithFields;
     this.createDetailsAssetForm(this.formBuilder);
+    this.menuActions = [{ label: 'Edit item', icon: 'pi pi-fw pi-pencil', command: (_) => { this.showEditDialog(); } },
+      { label: 'Assign Asset to room', icon: 'pi pw-fw pi-sign-in', command: (_) => { this.openAssignRoomDialog(); } },
+      { label: 'Delete', icon: 'pi pw-fw pi-trash', command: (_) => { this.showDeleteDialog(); } }];
+  }
+
+  ngOnChanges(): void {
+    this.displayedFactoryAssets = this.searchedFactoryAssets = this.filteredFactoryAssets = this.factoryAssetDetailsWithFields;
+  }
+
+  setActiveRow(asset?) {
+    if (asset) {
+      this.activeListItem = asset;
+    } else {
+      this.activeListItem = this.selectedFactoryAssets[0];
+    }
   }
 
   showOnboardDialog() {
@@ -111,7 +149,7 @@ export class AssetsListComponent implements OnInit {
     });
   }
 
-  createDetailsAssetForm(formBuilder: FormBuilder) {
+  createDetailsAssetForm(formBuilder: FormBuilder, factoryAsset?: FactoryAssetDetailsWithFields) {
     this.assetDetailsForm = formBuilder.group({
       id: [null],
       roomId: ['', WizardHelper.requiredTextValidator],
@@ -124,6 +162,9 @@ export class AssetsListComponent implements OnInit {
       roomName: ['', WizardHelper.requiredTextValidator],
       factorySiteName: ['', WizardHelper.requiredTextValidator]
     });
+    if (factoryAsset) {
+      this.assetDetailsForm.patchValue(factoryAsset);
+    }
   }
 
   assetUpdated(newAssetDetails: FactoryAssetDetails): void {
@@ -136,32 +177,6 @@ export class AssetsListComponent implements OnInit {
     return this.rooms.filter(room => room.id === roomId).pop();
   }
 
-  isSelected(id: ID) {
-    return this.selectedIds.has(id);
-  }
-
-  onAssetSelect(asset: FactoryAssetDetailsWithFields) {
-    this.selectedIds.add(asset.id);
-    this.emitSelectEvent();
-  }
-
-  onAssetDeselect(asset: FactoryAssetDetailsWithFields) {
-    this.selectedIds.delete(asset.id);
-    this.emitSelectEvent();
-  }
-
-  unselect() {
-    this.selectedIds.clear();
-  }
-
-  emitSelectEvent() {
-    this.selectedEvent.emit(this.selectedIds);
-  }
-
-  onFilter(filterDict: { [key: string]: string[]; }) {
-    this.filterDict = Object.assign({ }, filterDict);
-  }
-
   getRoomsLink() {
     if (this.room) {
       return ['..'];
@@ -171,18 +186,112 @@ export class AssetsListComponent implements OnInit {
   }
 
   onCardsViewClick() {
+    const selectedFactoryAssetIds = this.selectedFactoryAssets.map(asset => asset.id);
+    this.selectedEvent.emit(selectedFactoryAssetIds);
     this.toolBarClickEvent.emit('GRID');
   }
 
-
-  deleteAsset(event: FactoryAssetDetailsWithFields) {
-    this.assetService.removeCompanyAsset(event.companyId, event.id).subscribe(() => {
-      this.factoryAssetDetailsWithFields.splice(this.factoryAssetDetailsWithFields.indexOf(event), 1);
+  deleteAsset() {
+    this.assetService.removeCompanyAsset(this.activeListItem.companyId, this.activeListItem.id).subscribe(() => {
+      this.factoryAssetDetailsWithFields.splice(this.factoryAssetDetailsWithFields.indexOf(this.activeListItem), 1);
     });
   }
 
   goBack() {
     this.routingLocation.back();
+  }
+
+  searchAssets(event?): void {
+    this.searchedFactoryAssets = event;
+    this.updateAssets();
+  }
+
+  filterAssets(event?) {
+    this.filteredFactoryAssets = event;
+    this.updateAssets();
+  }
+
+  private updateAssets(): void {
+    this.displayedFactoryAssets = this.factoryAssetDetailsWithFields;
+    if (this.searchedFactoryAssets) {
+      this.displayedFactoryAssets = this.filteredFactoryAssets.filter(notification =>
+        this.searchedFactoryAssets.includes(notification));
+    }
+  }
+
+  showEditDialog() {
+    this.createDetailsAssetForm(this.formBuilder, this.activeListItem);
+    const ref = this.dialogService.open(AssetInstantiationComponent, {
+      data: {
+        assetDetailsForm: this.assetDetailsForm,
+        assetToBeEdited: this.activeListItem,
+        factorySites: this.factorySites,
+        factorySite: this.factorySite,
+        rooms: this.rooms,
+        activeModalType: AssetModalType.customizeAsset,
+        activeModalMode: AssetModalMode.editAssetMode
+      },
+      header: 'General Information',
+    });
+
+    ref.onClose.subscribe((assetFormValues: FactoryAssetDetails) => {
+      if (assetFormValues) {
+        this.assetUpdated(assetFormValues);
+      }
+    });
+  }
+
+  openAssignRoomDialog() {
+    if (this.factorySite) {
+      this.showAssignRoomDialog(AssetModalType.roomAssignment, AssetModalMode.editRoomWithPreselecedFactorySiteMode,
+        'Room Assignment (' + this.factorySite.name + ')');
+    } else {
+      this.showAssignRoomDialog(AssetModalType.factorySiteAssignment, AssetModalMode.editRoomForAssetMode,
+        'Factory Site Assignment');
+    }
+  }
+
+  showAssignRoomDialog(assetModalType: AssetModalType, assetModalMode: AssetModalMode, header: string) {
+    this.createDetailsAssetForm(this.formBuilder, this.activeListItem);
+    const ref = this.dialogService.open(AssetInstantiationComponent, {
+      data: {
+        assetDetailsForm: this.assetDetailsForm,
+        assetToBeEdited: this.activeListItem,
+        factorySites: this.factorySites,
+        factorySite: this.factorySite,
+        rooms: this.rooms,
+        activeModalType: assetModalType,
+        activeModalMode: assetModalMode
+      },
+      header
+    });
+
+    ref.onClose.subscribe((newAssetDetails: FactoryAssetDetails) => {
+      if (newAssetDetails) {
+        this.assetUpdated(newAssetDetails);
+      }
+    });
+  }
+
+  deselectAllItems(): void {
+    this.selectedFactoryAssets = [];
+  }
+
+  getAssetLink(asset: Asset) {
+    return ['/factorymanager', 'companies', asset.companyId, 'assets', asset.id];
+  }
+
+  showDeleteDialog() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete the asset ' + this.activeListItem.name + '?',
+      header: 'Delete Asset Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.deleteAsset();
+      },
+      reject: () => {
+      }
+    });
   }
 }
 
--- fusionfrontend/src/app/factory/factory.module.ts
+++ fusionfrontend/src/app/factory/factory.module.ts
@@ -39,8 +39,6 @@ import { FactorySubHeaderComponent } from './components/content/factory-sub-head
 import { PrecisionPipe } from '../pipes/precision.pipe';
 import { StatusComponent } from './components/content/status/status.component';
 import { ArraysortextendedPipe } from '../pipes/arraysortextended.pipe';
-import { AssetsListItemComponent } from './components/content/assets-list/assets-list-item/assets-list-item.component';
-import { AssetsListHeaderComponent } from './components/content/assets-list/assets-list-header/assets-list-header.component';
 import { ArrayFilterPipe } from '../pipes/tablefilter.pipe';
 import { ListFilterComponent } from './components/content/assets-list/list-filter/list-filter.component';
 import { AssetInstantiationComponent } from './components/content/asset-instantiation/asset-instantiation.component';
@@ -95,8 +93,6 @@ import { AssetNotificationsComponent } from './components/pages/asset-details/as
     StatusComponent,
     FactorySitesListHeaderComponent,
     FactorySitesListItemComponent,
-    AssetsListItemComponent,
-    AssetsListHeaderComponent,
     ListFilterComponent,
     AssetInstantiationComponent,
     AssetInstantiationStartModalComponent,
