885ea71cf7e8ae2634e39708e2be1b5689c416eb
Johannes Sylupp <johannes.sylupp@iteratec.com>
2021-10-12 13:26:13 +0200

    IF-436: Fix pmd violation

--- fusionbackend/src/main/java/io/fusion/fusionbackend/service/FieldInstanceService.java
+++ fusionbackend/src/main/java/io/fusion/fusionbackend/service/FieldInstanceService.java
@@ -27,7 +27,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Locale;
 
 @Service
 @Transactional
