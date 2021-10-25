9b54148eb3baf9b0f35a6ab45e1f781731920a21
Samuel Sedlmeir <Samuel.Sedlmeir@iteratec.com>
2021-10-25 08:29:10 +0200

    IF-477: move asset details context menu actions to dedicated service

--- fusionfrontend/src/app/factory/components/content/assets-list/assets-list.component.ts
+++ fusionfrontend/src/app/factory/components/content/assets-list/assets-list.component.ts
@@ -27,16 +27,16 @@ import {
   FactoryAssetDetailsWithFields
 } from '../../../../store/factory-asset-details/factory-asset-details.model';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { Asset, AssetWithFields } from '../../../../store/asset/asset.model';
 import { AssetInstantiationComponent } from '../asset-instantiation/asset-instantiation.component';
-import { WizardHelper } from '../../../../common/utils/wizard-helper';
 import { ConfirmationService, TreeNode } from 'primeng/api';
 import { FilterOption, FilterType } from '../../../../components/ui/table-filter/filter-options';
 import { ItemOptionsMenuType } from 'src/app/components/ui/item-options-menu/item-options-menu.type';
 import { TableSelectedItemsBarType } from '../../../../components/ui/table-selected-items-bar/table-selected-items-bar.type';
 import { OispAlert, OispAlertPriority } from '../../../../store/oisp/oisp-alert/oisp-alert.model';
 import { faExclamationCircle, faExclamationTriangle, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { AssetDetailMenuService } from '../../../../services/asset-detail-menu.service';
 
 @Component({
   selector: 'app-assets-list',
@@ -91,22 +91,25 @@ export class AssetsListComponent implements OnInit, OnChanges {
   ItemOptionsMenuType = ItemOptionsMenuType;
   TableSelectedItemsBarType = TableSelectedItemsBarType;
 
-  tableFilters: FilterOption[] = [{ filterType: FilterType.DROPDOWNFILTER, columnName: 'Category', attributeToBeFiltered: 'category' },
+  tableFilters: FilterOption[] = [{
+    filterType: FilterType.DROPDOWNFILTER,
+    columnName: 'Category',
+    attributeToBeFiltered: 'category'
+  },
     { filterType: FilterType.DROPDOWNFILTER, columnName: 'Manufacturer', attributeToBeFiltered: 'manufacturer' },
     { filterType: FilterType.DROPDOWNFILTER, columnName: 'Room', attributeToBeFiltered: 'roomName' },
-    { filterType: FilterType.DROPDOWNFILTER, columnName: 'Factory Site', attributeToBeFiltered: 'factorySiteName'}];
+    { filterType: FilterType.DROPDOWNFILTER, columnName: 'Factory Site', attributeToBeFiltered: 'factorySiteName' }];
 
   constructor(
     private assetService: AssetService,
-    private formBuilder: FormBuilder,
     private dialogService: DialogService,
-    private confirmationService: ConfirmationService) {
-      this.createDetailsAssetForm(this.formBuilder);
+    private assetDetailMenuService: AssetDetailMenuService) {
   }
 
   ngOnInit() {
-    this.createDetailsAssetForm(this.formBuilder);
+    this.assetDetailsForm = this.assetDetailMenuService.createAssetDetailsForm();
   }
+
   ngOnChanges(): void {
     this.displayedFactoryAssets = this.searchedFactoryAssets = this.filteredFactoryAssets = this.factoryAssetDetailsWithFields;
     this.updateTree();
@@ -128,15 +131,6 @@ export class AssetsListComponent implements OnInit, OnChanges {
   filterAssets(event?: FactoryAssetDetailsWithFields[]) {
     this.filteredFactoryAssets = event;
     this.updateAssets();
-  }
-
-  private updateAssets(): void {
-    this.displayedFactoryAssets = this.factoryAssetDetailsWithFields;
-    if (this.searchedFactoryAssets) {
-      this.displayedFactoryAssets = this.filteredFactoryAssets.filter(asset =>
-        this.searchedFactoryAssets.includes(asset));
-    }
-    this.updateTree();
   }
 
   public getMaxOpenAlertPriority(node: TreeNode<FactoryAssetDetailsWithFields>): OispAlertPriority {
@@ -161,6 +155,95 @@ export class AssetsListComponent implements OnInit, OnChanges {
     } else {
       return false;
     }
+  }
+
+  showOnboardDialog() {
+    const ref = this.dialogService.open(AssetInstantiationComponent, {
+      data: {
+        assetDetailsForm: this.assetDetailsForm,
+        assetsToBeOnboarded: this.factoryAssetDetailsWithFields,
+        factorySites: this.factorySites,
+        rooms: this.rooms,
+        activeModalType: AssetModalType.startInitialization,
+        activeModalMode: AssetModalMode.onboardAssetMode
+      },
+      header: 'Select Asset for Onboarding',
+      width: '70%',
+      contentStyle: { 'padding-left': '6%', 'padding-right': '6%', 'padding-top': '1.5%' },
+    });
+
+    ref.onClose.subscribe((assetFormValues: FactoryAssetDetails) => {
+      if (assetFormValues) {
+        this.assetUpdated(assetFormValues);
+      }
+    });
+  }
+
+  assetUpdated(newAssetDetails: FactoryAssetDetails): void {
+    const oldRoom = this.getOldRoomForAsset(newAssetDetails);
+    this.updateAssetEvent.emit([oldRoom, newAssetDetails]);
+  }
+
+  getOldRoomForAsset(updatedAsset) {
+    const roomId = this.factoryAssetDetailsWithFields.filter(asset => asset.id === updatedAsset.id).pop().roomId;
+    return this.rooms.filter(room => room.id === roomId).pop();
+  }
+
+  onCardsViewClick() {
+    const selectedFactoryAssetIds = this.selectedFactoryAssets.map(asset => asset.data.id);
+    this.selectedEvent.emit(selectedFactoryAssetIds);
+    this.toolBarClickEvent.emit('GRID');
+  }
+
+  deleteAsset() {
+    this.assetService.removeCompanyAsset(this.activeListItem.companyId, this.activeListItem.id).subscribe(() => {
+      this.factoryAssetDetailsWithFields.splice(this.factoryAssetDetailsWithFields.indexOf(this.activeListItem), 1);
+    });
+  }
+
+  showEditDialog() {
+    this.assetDetailMenuService.showEditDialog(this.activeListItem, this.factorySite, this.factorySites, this.rooms,
+      () => this.onCloseEditDialog(), (details) => this.assetUpdated(details));
+  }
+
+  onCloseEditDialog() {
+    this.selectedFactoryAssets = [];
+  }
+
+  openAssignRoomDialog() {
+    if (this.factorySite) {
+      this.showAssignRoomDialog(AssetModalType.roomAssignment, AssetModalMode.editRoomWithPreselecedFactorySiteMode,
+        `Room Assignment (${this.factorySite.name})`);
+    } else {
+      this.showAssignRoomDialog(AssetModalType.factorySiteAssignment, AssetModalMode.editRoomForAssetMode,
+        'Factory Site Assignment');
+    }
+  }
+
+  deselectAllItems(): void {
+    this.selectedFactoryAssets = [];
+  }
+
+  getAssetLink(asset: Asset) {
+    return ['/factorymanager', 'companies', asset.companyId, 'assets', asset.id];
+  }
+
+  showDeleteDialog() {
+    this.assetDetailMenuService.showDeleteDialog(this.activeListItem.name, () => this.deleteAsset());
+  }
+
+  private showAssignRoomDialog(modalType: AssetModalType, modalMode: AssetModalMode, header: string) {
+    this.assetDetailMenuService.showAssignRoomDialog(this.activeListItem, this.factorySite, this.factorySites,
+      this.rooms, modalType, modalMode, header, (details) => this.assetUpdated(details));
+  }
+
+  private updateAssets(): void {
+    this.displayedFactoryAssets = this.factoryAssetDetailsWithFields;
+    if (this.searchedFactoryAssets) {
+      this.displayedFactoryAssets = this.filteredFactoryAssets.filter(asset =>
+        this.searchedFactoryAssets.includes(asset));
+    }
+    this.updateTree();
   }
 
   private updateTree() {
@@ -206,146 +289,5 @@ export class AssetsListComponent implements OnInit, OnChanges {
       treeNode.children = children;
     }
     return treeNode;
-  }
-
-
-  showOnboardDialog() {
-    const ref = this.dialogService.open(AssetInstantiationComponent, {
-      data: {
-        assetDetailsForm: this.assetDetailsForm,
-        assetsToBeOnboarded: this.factoryAssetDetailsWithFields,
-        factorySites: this.factorySites,
-        rooms: this.rooms,
-        activeModalType: AssetModalType.startInitialization,
-        activeModalMode: AssetModalMode.onboardAssetMode
-      },
-      header: 'Select Asset for Onboarding',
-      width: '70%',
-      contentStyle: { 'padding-left': '6%', 'padding-right': '6%', 'padding-top': '1.5%' },
-    });
-
-    ref.onClose.subscribe((assetFormValues: FactoryAssetDetails) => {
-      if (assetFormValues) {
-        this.assetUpdated(assetFormValues);
-      }
-    });
-  }
-
-  // TODO: Has to be extracted into Dialog/AssetInstantiationComponent (IF-429)
-  createDetailsAssetForm(formBuilder: FormBuilder, factoryAsset?: FactoryAssetDetailsWithFields) {
-    this.assetDetailsForm = formBuilder.group({
-      id: [null],
-      version: [],
-      roomId: ['', WizardHelper.requiredTextValidator],
-      name: ['', WizardHelper.requiredTextValidator],
-      description: [''],
-      imageKey: [''],
-      manufacturer: ['', WizardHelper.requiredTextValidator],
-      assetSeriesName: ['', WizardHelper.requiredTextValidator],
-      category: ['', WizardHelper.requiredTextValidator],
-      roomName: ['', WizardHelper.requiredTextValidator],
-      factorySiteName: ['', WizardHelper.requiredTextValidator]
-    });
-    if (factoryAsset) {
-      this.assetDetailsForm.patchValue(factoryAsset);
-    }
-  }
-
-  assetUpdated(newAssetDetails: FactoryAssetDetails): void {
-    const oldRoom = this.getOldRoomForAsset(newAssetDetails);
-    this.updateAssetEvent.emit([oldRoom, newAssetDetails]);
-  }
-
-  getOldRoomForAsset(updatedAsset) {
-    const roomId = this.factoryAssetDetailsWithFields.filter(asset => asset.id === updatedAsset.id).pop().roomId;
-    return this.rooms.filter(room => room.id === roomId).pop();
-  }
-
-  onCardsViewClick() {
-    const selectedFactoryAssetIds = this.selectedFactoryAssets.map(asset => asset.data.id);
-    this.selectedEvent.emit(selectedFactoryAssetIds);
-    this.toolBarClickEvent.emit('GRID');
-  }
-
-  deleteAsset() {
-    this.assetService.removeCompanyAsset(this.activeListItem.companyId, this.activeListItem.id).subscribe(() => {
-      this.factoryAssetDetailsWithFields.splice(this.factoryAssetDetailsWithFields.indexOf(this.activeListItem), 1);
-    });
-  }
-
-  showEditDialog() {
-    this.createDetailsAssetForm(this.formBuilder, this.activeListItem);
-    const ref = this.dialogService.open(AssetInstantiationComponent, {
-      data: {
-        assetDetailsForm: this.assetDetailsForm,
-        assetToBeEdited: this.activeListItem,
-        factorySites: this.factorySites,
-        factorySite: this.factorySite,
-        rooms: this.rooms,
-        activeModalType: AssetModalType.customizeAsset,
-        activeModalMode: AssetModalMode.editAssetMode
-      },
-      header: 'General Information',
-    });
-
-    ref.onClose.subscribe((assetFormValues: FactoryAssetDetails) => {
-      this.selectedFactoryAssets = [];
-      if (assetFormValues) {
-        this.assetUpdated(assetFormValues);
-      }
-    });
-  }
-
-  openAssignRoomDialog() {
-    if (this.factorySite) {
-      this.showAssignRoomDialog(AssetModalType.roomAssignment, AssetModalMode.editRoomWithPreselecedFactorySiteMode,
-        'Room Assignment (' + this.factorySite.name + ')');
-    } else {
-      this.showAssignRoomDialog(AssetModalType.factorySiteAssignment, AssetModalMode.editRoomForAssetMode,
-        'Factory Site Assignment');
-    }
-  }
-
-  showAssignRoomDialog(assetModalType: AssetModalType, assetModalMode: AssetModalMode, header: string) {
-    this.createDetailsAssetForm(this.formBuilder, this.activeListItem);
-    const ref = this.dialogService.open(AssetInstantiationComponent, {
-      data: {
-        assetDetailsForm: this.assetDetailsForm,
-        assetToBeEdited: this.activeListItem,
-        factorySites: this.factorySites,
-        factorySite: this.factorySite,
-        rooms: this.rooms,
-        activeModalType: assetModalType,
-        activeModalMode: assetModalMode
-      },
-      header
-    });
-
-    ref.onClose.subscribe((newAssetDetails: FactoryAssetDetails) => {
-      if (newAssetDetails) {
-        this.assetUpdated(newAssetDetails);
-      }
-    });
-  }
-
-  deselectAllItems(): void {
-    this.selectedFactoryAssets = [];
-  }
-
-  getAssetLink(asset: Asset) {
-    return ['/factorymanager', 'companies', asset.companyId, 'assets', asset.id];
-  }
-
-  showDeleteDialog() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete the asset ' + this.activeListItem.name + '?',
-      header: 'Delete Asset Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.deleteAsset();
-      },
-      reject: () => {
-      }
-    });
   }
 }
