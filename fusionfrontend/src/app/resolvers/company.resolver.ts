9273ce44c4f303ea7ceb137c8cf2e99f9d255cb4
Johannes Sylupp <johannes.sylupp@iteratec.com>
2021-10-27 14:12:23 +0200

    IF-504: Add company resolver

---
+++ fusionfrontend/src/app/resolvers/company.resolver.ts
@@ -0,0 +1,33 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+
+import { CompanyService } from '../store/company/company.service';
+import { RouteHelpers } from '../common/utils/route-helpers';
+import { tap } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class CompanyResolver implements Resolve<any>{
+  constructor(private companyService: CompanyService) { }
+
+  resolve(route: ActivatedRouteSnapshot): void { // using Observable will crash routing
+    this.companyService.getCompanies().pipe(tap(() => {
+      const companyId = RouteHelpers.findParamInFullActivatedRoute(route, 'companyId');
+      this.companyService.setActive(companyId);
+    })).subscribe();
+  }
+}
