35ae7e3b727301c28c206ebd193ee5c95ec86b17
jsylupp <johannes.sylupp@iteratec.com>
2021-12-01 09:44:23 +0100

    Update ticket number

--- fusionbackend/src/main/resources/db/migration/V21__DT-xx_add_asset_type_template_subsystems.sql
+++
@@ -1,4 +0,0 @@
-alter table asset_type_template
-    add column subsystem_parent_id bigint;
-alter table asset_type_template
-    add constraint asset_type_template_subsystem_parent_id_fkey foreign key (subsystem_parent_id) references asset_type_template;
