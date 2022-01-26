abe1e56ffb6ac4d5a9facf603f0dbac02fc855ce
David Lane <david.lane@iteratec.com>
2022-01-26 11:44:42 +0100

    Quickfix for frontend error

--- fusionfrontend/src/app/core/services/api/if-api.service.ts
+++ fusionfrontend/src/app/core/services/api/if-api.service.ts
@@ -27,7 +27,7 @@ export class IfApiService {
     headers: new HttpHeaders({ Accept: 'application/zip' })
   };
   syncResult: SyncResult = new SyncResult();
-  showResult: boolean;
+  showResult: boolean = false;
 
   constructor(private http: HttpClient) {
   }
